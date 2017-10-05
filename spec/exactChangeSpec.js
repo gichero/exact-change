92e60030efb13e06d20b842fbe363c1526dbdc38
Amos <amosgathungu@gmail.com>
2017-10-04 21:20:08 -0400

    algorithm challenge (jasmine testing)

--- spec/exactChangeSpec.js
+++ spec/exactChangeSpec.js
@@ -1,4 +1,4 @@
-describe ('change', function(){
+describe ('Result', function(){
 
     it('should return insufficient funds when the cashInDrawer is less than change', function(){
         var result = checkCashRegister(5.00, 20.00, [['TEN', 10.00]]);
@@ -9,16 +9,23 @@ describe ('change', function(){
         expect(result).toEqual('Closed');
     })
     it('should not return change where it is not required', function(){
-        var result = checkCashRegister(10.00, 10.00, [['ONE HUNDRED', 100.00]]);
+        var result = checkCashRegister(10.00, 10.00, [['TEN', 10.00]]);
         expect(result).toEqual('insufficient funds');
     })
     it('should return the correct change', function(){
-        var result = checkCashRegister(30.00, 40.00, [['FIVE', 10.00], ['ONE', 7.00], ['QUARTER', 2.25]])
+        var result = checkCashRegister(30.00, 40.00, [['FIVE', 10.00], ['ONE', 7.00], ['QUARTER', 2.25]]);
         expect(result).toEqual([['FIVE', 10.00]]);
     })
-    it("returns invalid input when the price is a negative number", function(){
-        var result = checkCashRegister(-30.00, 40.00, [['FIVE', 10.00], ['ONE', 7.00], ['QUARTER', 2.25]]);
-        expect(result).toEqual('Invalid Input');
+    it('should return no change when the price equals the cash', function(){
+        var result = checkCashRegister(5.00, 5.00, [['TWENTY', 100.00], ['TEN', 40.00], ['FIVE', 25.00], ['ONE', 11.00], ['QUARTER', 5.00]]);
+        expect(result).toEqual('No Change');
     })
+    it("should return invalid input if price and/or cash are negative numbers", function() {
 
+        var result = "Invalid Input";
+
+        expect(checkCashRegister(-30.00, 40.00, [['FIVE', 10.00], ['ONE', 7.00], ['QUARTER', 2.25]])).toEqual(result);
+        expect(checkCashRegister(30.00, -40.00, [['FIVE', 10.00], ['ONE', 7.00], ['QUARTER', 2.25]])).toEqual(result);
+        expect(checkCashRegister(-30.00, -40.00, [['FIVE', 10.00], ['ONE', 7.00], ['QUARTER', 2.25]])).toEqual(result);
+  })
 })
