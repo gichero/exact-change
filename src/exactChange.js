cf12a6dddfb6a82fdfb58683775d96ecb12b6d24
Amos <amosgathungu@gmail.com>
2017-11-25 23:22:08 -0500

    refactored code

--- src/exactChange.js
+++ src/exactChange.js
@@ -157,65 +157,66 @@ function noChange(drawer, change){
 //
 // What would be a better way to match the denominations with cashInDrawer without using the zip method. For example, the algorithm should be flexible enough to where a user may leave out a denomination in the input.
 
-//solution from freecodecamp
-// Create an object which hold the denominations and their values
-var denom = [
-  { name: 'ONE HUNDRED', val: 100.00},
-  { name: 'TWENTY', val: 20.00},
-  { name: 'TEN', val: 10.00},
-  { name: 'FIVE', val: 5.00},
-  { name: 'ONE', val: 1.00},
-  { name: 'QUARTER', val: 0.25},
-  { name: 'DIME', val: 0.10},
-  { name: 'NICKEL', val: 0.05},
-  { name: 'PENNY', val: 0.01}
-];
+//submitted code
+var denominations = [
+    {name: 'ONE HUNDRED', value: 100.00},
+    {name: 'TWENTY',      value: 20.00},
+    {name: 'TEN',         value: 10.00},
+    {name: 'FIVE',        value: 5.00},
+    {name: 'ONE',         value: 1.00},
+    {name: 'QUARTER',     value: 0.25},
+    {name: 'DIME',        value: 0.10},
+    {name: 'NICKEL',      value: 0.05},
+    {name: 'PENNY',       value: 0.01}
+    ];
 
-function checkCashRegister(price, cash, cid) {
-  var change = cash - price;
+function checkCashRegister(price, cash, cashInDrawer){
 
-  // Transform CID array into drawer object
-  var register = cid.reduce(function(acc, curr) {
-    acc.total += curr[1];
-    acc[curr[0]] = curr[1];
-    return acc;
-  }, {total: 0});
+        var change = cash - price;
 
-  // Handle exact change
-  if (register.total === change) {
-    return 'Closed';
-  }
+        var drawer = cashInDrawer.reduce(function(accumulator, currentValue){
+            accumulator.total += currentValue[1];
+            accumulator[currentValue[0]] = currentValue[1];
+            return accumulator;
+        }, {total: 0.0});
 
-  // Handle obvious insufficent funds
-  if (register.total < change) {
-    return 'Insufficient Funds';
-  }
+        if(drawer.total < change){
+            return 'Insufficient Funds';
+        }
+        if(drawer.total === change){
+            return 'Closed';
+        }
+        if(price < 0){
+            return 'Invalid Entry';
+        }
+        if(cash < 0){
+            return 'Invalid Entry';
+        }
+        if(change == 0 && drawer.total > 0){
+            return 'No Change';
+        }
 
-  // Loop through the denomination array
-  var change_arr = denom.reduce(function(acc, curr) {
-    var value = 0;
-    // While there is still money of this type in the drawer
-    // And while the denomination is larger than the change reminaing
-    while (register[curr.name] > 0 && change >= curr.val) {
-      change -= curr.val;
-      register[curr.name] -= curr.val;
-      value += curr.val;
+        var result = denominations.reduce(function(accumulator, currentValue){
+            var val = 0;
 
-      // Round change to the nearest hundreth deals with precision errors
-      change = Math.round(change * 100) / 100;
-    }
-    // Add this denomination to the output only if any was used.
-    if (value > 0) {
-        acc.push([ curr.name, value ]);
-    }
-    return acc; // Return the current Change Array
-  }, []); // Initial value of empty array for reduce
+            while(drawer[currentValue.name] > 0  && change >= currentValue.value){
+                change -= currentValue.value;
+                drawer[currentValue.name] -= currentValue.value;
+                val += currentValue.value;
+                change = Math.round(change * 100) / 100;
+            }
 
-  // If there are no elements in change_arr or we have leftover change, return
-  // the string "Insufficient Funds"
-  if (change_arr.length < 1 || change > 0) {
-    return "Insufficient Funds";
-  }
+            if(val > 0){
+                accumulator.push([currentValue.name, val]);
+            }
 
-  // Here is your change, ma'am.
-  return change_arr;
+            return accumulator;
+
+        },[]);
+
+        if(result.length < 1 || change > 0){
+            return 'Insufficient Funds';
+        }
+        return result;
+}
+ checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
